d6a8c65d70f5bd5588a83a52d480865ea640c3d2
Elton Rodrigues <elton.rodrigues@rededor.com.br>
2022-11-03 20:50:29 -0300

    refactor: ajuste para react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import Header from './components/Header'
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root')!)
+
+root.render(
   <React.StrictMode>
     <Header />
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
